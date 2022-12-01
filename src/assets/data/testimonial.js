98cff2c4d4a4fa5b49a565f6071d76777fbb82ea
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-12-01 22:09:49 +0700

    add data for testimonial

---
+++ src/assets/data/testimonial.js
@@ -0,0 +1,23 @@
+export const testimonial = [
+  {
+    id: 0,
+    name: "Rizki Haddi Prayoga",
+    position: "CEO GunTour",
+    content: "",
+    // img: img_testimonials,
+  },
+  {
+    id: 1,
+    name: "Rizki Haddi Prayoga",
+    position: "CEO GunTour",
+    content: "",
+    // img: img_testimonials,
+  },
+  {
+    id: 2,
+    name: "Rizki Haddi Prayoga",
+    position: "CEO GunTour",
+    content: "",
+    // img: img_testimonials,
+  },
+];
