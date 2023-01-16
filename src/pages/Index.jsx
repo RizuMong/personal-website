6885084505b36ff0c8d5e9601d77aa0f88451058
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2023-01-16 09:43:42 +0700

    update

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -176,15 +176,15 @@ const Index = () => {
             </div>
           </div>
           <div className="px-4 md:px-6 lg:px-20 grid md:grid-cols-3 grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 md:gap-6 lg:gap-9 justify-items-center xl:justify-center">
-            {testimonial.map((data) => {
+            {testimonial.map((data) => (
               <CardTestimonial
                 key={data.id}
                 content={data.content}
                 src={data.img}
                 name={data.name}
                 position={data.position}
-              />;
-            })}
+              />
+            ))}
           </div>
         </div>
 
